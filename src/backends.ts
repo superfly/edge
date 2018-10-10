330bad840ad6356eabae97cb86e73be5b2960bbb
Kurt <mrkurt@gmail.com>
2018-10-10 13:32:04 -0500

    support for load balanced backends

--- src/backends.ts
+++ src/backends.ts
@@ -1,7 +1,8 @@
 import proxy from "@fly/fetch/proxy"
+import balancer from "@fly/load-balancer"
 
 export interface BackendInfo {
-  origin: string, // was upstream,
+  origin: string | string[], // was upstream,
   type?: string,
   headers?: { [name: string]: string | boolean | undefined }
 }
@@ -16,9 +17,21 @@ export default function backend(backend: BackendInfo): Backend {
   //TODO: Implement s3, dropbox, lamdba, etc
   console.log("Proxying:", backend)
   const b = notImplemented.filter((t) => t === backend.type).length == 0 ?
-    proxy(backend.origin, { forwardHostHeader: true, headers: backend.headers }) :
+    originFetch(backend) :
     unsupportedBackend(backend.type || "default")
   return Object.assign(b, { info: backend })
+}
+
+function originFetch(backend: BackendInfo){
+  if(typeof backend.origin === "string"){
+    return proxy(backend.origin, { forwardHostHeader: true, headers: backend.headers })
+  }
+  if(backend.origin instanceof Array){
+    const backends = backend.origin.map((o) =>{
+      return proxy(o, { forwardHostHeader: true, headers: backend.headers })
+    })
+    return balancer(backends)
+  }
 }
 
 function unsupportedBackend(type: string) {
