3ac54ae3e673cae6e234ce086d773d431150b093
Kurt <mrkurt@gmail.com>
2018-12-14 11:34:35 -0600

    Export middleware for lib

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ import { FlyRequest } from "@fly/fetch";
 
 export { BackendInfo, BackendMap } from "./backends"
 export { RuleInfo } from "./rules"
+import * as middleware from "./middleware"
 export { MiddlewareConfig } from "./middleware"
+export { middleware }
+
 export { SiteConfig } from "./site"
 
 /**
