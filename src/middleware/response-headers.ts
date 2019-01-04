aa2028fb3c33edb84e6fa4a0ccfef72f62725a6f
Kurt <mrkurt@gmail.com>
2019-01-03 21:45:56 -0600

    better responseHeaders tests

--- src/middleware/response-headers.ts
+++ src/middleware/response-headers.ts
@@ -1,14 +1,25 @@
 /**
  * @module Middleware
  */
-import * as builder from "./builder";
+import { responseModifier } from "./builder";
 
+/**
+ * Header name/value pairs to set on a response. The boolean `false` removes the header entirely.
+ */
 export interface ResponseHeadersOptions {
-  [name: string]: string | boolean
+  [name: string]: string | false
 }
 
 /**
+ * Middleware to set headers on responses
+ * @param fetch 
+ * @param options 
+ */
+export const responseHeaders = responseModifier(addResponseHeaders)
+
+/**
  * Sets provided headers on a response object
+ * @private
  * @param resp 
  * @param options 
  */
@@ -17,18 +28,10 @@ export async function addResponseHeaders(resp: Response, headers: ResponseHeader
     for (const [k, v] of Object.entries(headers)) {
       if (v === false) {
         resp.headers.delete(k)
-      }
-      if (v) {
+      } else if (<any>v !== true) { // true implies pass through
         resp.headers.set(k, v.toString())
       }
     }
   }
   return resp
 }
-
-/**
- * Middleware to set headers on responses
- * @param fetch 
- * @param options 
- */
-export const responseHeaders = builder.responseModifier(addResponseHeaders)
--- test/middleware/response-headers.spec.ts
+++ test/middleware/response-headers.spec.ts
@@ -4,9 +4,19 @@ import { echo } from "../../src/backends";
 
 
 describe("middleware/responseHeaders", function() {
-  it("works as middleware", async ()=>{
+  it("sets a header with a string value", async ()=>{
     const fn = responseHeaders(echo, { "Powered-By": "Caffeine" });
     const resp = await fn("http://wat/")
     expect(resp.headers.get("powered-by")).to.eq("Caffeine")
+  })
+  it("removes a header with a `false` value", async ()=>{
+    const fn = responseHeaders(echo, { "content-type": false });
+    const resp = await fn("http://wat/")
+    expect(resp.headers.get("content-type")).to.be.null
+  })
+  it("ignores a header setting with a `true` value", async ()=>{
+    const fn = responseHeaders(echo, <any>{ "content-type": true });
+    const resp = await fn("http://wat/")
+    expect(resp.headers.get("content-type")).to.eq("application/json")
   })
 })
