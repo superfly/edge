61b0ad4fc964b4ad9e055961522f025b96c6bd3a
kittybot <christinakeelan@gmail.com>
2019-01-14 15:18:04 -0600

    revert unrelated changes

---
+++ src/backends.ts
@@ -0,0 +1,11 @@
+/**
+ * @module Backends
+ */
+export * from "./backends/origin";
+export * from "./backends/glitch";
+export * from "./backends/github_pages";
+export * from "./backends/ghost_pro";
+export * from "./backends/heroku";
+export * from "./backends/netlify";
+export * from "./backends/echo";
+export * from "./backends/squarespace";
