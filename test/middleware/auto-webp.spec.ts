1474cd80b984e2a2db59dd13092f4c9040b95f78
Kurt <mrkurt@gmail.com>
2019-01-15 17:41:22 -0600

    better webp middleware tests

--- test/middleware/auto-webp.spec.ts
+++ test/middleware/auto-webp.spec.ts
@@ -1,37 +1,50 @@
 import { expect } from "chai";
 import { autoWebp } from "../../src/middleware";
-import { origin } from "../../src/backends";
-import { echo } from "../../src/backends";
 
-const { Image } = require("@fly/image");
+const img = require("../fixtures/image.png");
 
-function contentType(resp: Response){
-	const type = resp.headers.get("content-type");
-	if (type !== "image/png" || "image/jpeg") {
-		return resp.status;
+async function mock(req: RequestInfo){
+	const url = new URL(typeof req === "string" ? req : req.url);
+
+	if(url.pathname === "/text"){
+		return new Response("this is some unaltered text", { headers: {"content-type": "text/plain"}});
 	}
+	if(url.pathname === "/image.png"){
+		return new Response(img, { headers: {"content-type": "image/png"}});
+	}
+	return new Response("not found", { status: 404 })
 }
 
 describe("middleware/autoWebp", function() {
 	it("successfully lets non-image responses pass through", async ()=>{
-		const backend = origin({
-		  origin: "https://fly.io/",
-		  headers: {host: "fly.io"}
-		});
-		const fn = autoWebp(backend);
-		const resp = await fn("https://fly.io/");
-		expect(contentType(resp)).to.eq(200);
+		const fetch = autoWebp(mock);
+		const resp = await fetch("https://testing/text");
+		expect(resp.status).to.eq(200);
+		expect(resp.headers.get("content-type")).to.eq("text/plain");
 	})
 
-	it("properly generates webp images", async ()=>{
-		const img = "https://fly.io/public/images/modern-javascript@2x.png";
-		const response = await fetch(img);
-		let imageData = await response.arrayBuffer();
-		const originalImage = new Image(imageData);
-    	const newImage = originalImage.webp();
-        const convertedImage = await newImage.toImage();
-
-        const meta = convertedImage.metadata();
-        expect(meta.format).to.eq("webp");
+	it("serves webp when accept includes image/webp", async ()=>{
+		const fetch = autoWebp(mock)
+		const resp = await fetch("https://testing/image.png", { 
+			headers: {
+				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" // a typical chrome accept header
+			}
+		})
+		const body = await resp.arrayBuffer();
+		expect(resp.status).to.eq(200)
+		expect(resp.headers.get("content-type")).to.eq("image/webp")
+		expect(body.byteLength).to.be.lessThan(img.byteLength, "webp image data should be smaller than source png")
+	})
+	it("doesn't serve webp when header is missing", async ()=>{
+		const fetch = autoWebp(mock)
+		const resp = await fetch("https://testing/image.png", { 
+			headers: {
+				"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" // a typical chrome accept header
+			}
+		})
+		const body = await resp.arrayBuffer();
+		expect(resp.status).to.eq(200)
+		expect(resp.headers.get("content-type")).to.eq("image/png")
+		expect(body.byteLength).to.be.eq(img.byteLength, "untouched image data should be the same as the original")
 	})
 })
