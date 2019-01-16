c5cafc23603986ef0fc5dc81d7d3e2f6aefeb8d7
Elise Barnes <barnese3@go.stockton.edu>
2019-01-15 19:49:11 -0500

    feat: Automatically serve webp images when possible (#17)

--- webpack.fly.config.js
+++ webpack.fly.config.js
@@ -3,7 +3,7 @@ let path = require('path')
 module.exports = {
   entry: "./index.ts",
   resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: ['.js', '.ts', '.tsx', '.png', '.jpg', '.gif', '.svg'],
     alias: {
       crypto: path.resolve(__dirname, 'src', 'shims', 'crypto'),
     }
@@ -13,6 +13,10 @@ module.exports = {
       {
         test: /\.tsx?$/,
         loader: 'ts-loader'
+      },
+      {
+        test: /\.(ico|svg|png|jpg|gif)$/,
+        use: ['arraybuffer-loader', 'image-webpack-loader']
       }
     ]
   }
