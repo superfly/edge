f3c77e64fbd76cbbd588fc235cceab13bb9f600e
Kurt Mackey <mrkurt@gmail.com>
2019-03-14 14:08:56 -0500

    feat: load balancing
    Latency and health aware load balancing across a list of backends.

---
+++ src/balancer.ts
@@ -0,0 +1,256 @@
+import { FetchFunction } from "./fetch";
+
+/**
+ * A fetch function load balancer. Distributes requests to a set of backends; attempts to 
+ * send requests to most recently healthy backends using a 2 random (pick two healthiest, 
+ * randomize which gets requests).
+ * 
+ * If all backends are healthy, tries to evenly distribute requests as much as possible.
+ * 
+ * When backends return server errors (500-599) it retries idempotent requests
+ *  until it gets a good response, or all backends have been tried.
+ * 
+ * @param backends fetch functions for each backend to balance accross
+ * @returns a function that behaves just like fetch, with a `.backends` property for 
+ * retrieving backend stats.
+ */
+export default function balancer(backends: FetchFunction[]) {
+  let tracked = syncBackends([], backends)
+
+  const fn = async function fetchBalancer(req: RequestInfo, init?: RequestInit | undefined): Promise<Response> {
+    if (typeof req === "string") {
+      req = new Request(req)
+    }
+    const url = new URL(req.url)
+    let trackLatency = url.pathname === "/" // this would be configurable in real life
+    const attempted = new Set<Backend>()
+    while (attempted.size < tracked.length) {
+      let backend: Backend | null = null
+      const [backendA, backendB] = chooseBackends(tracked, attempted)
+
+      if (!backendA) {
+        return new Response("No backend available", { status: 502 })
+      }
+      if (!backendB) {
+        backend = backendA
+      } else {
+        // randomize between 2 good candidates
+        backend = (Math.floor(Math.random() * 2) == 0) ? backendA : backendB
+      }
+
+      const promise = backend.proxy(req, init)
+      if (backend.scoredRequestCount != backend.requestCount) {
+        // fixup score
+        // this should be relatively concurrent with the fetch promise
+        scoreHealth(backend)
+      }
+      backend.requestCount += 1
+      attempted.add(backend)
+
+      const start = Date.now()
+      let resp: Response
+      try {
+        resp = await promise
+      } catch (e) {
+        resp = proxyError
+        trackLatency = false
+      }
+      setFixedArrayValue(backend.statuses, resp.status, 10, backend.requestCount)
+
+      if(trackLatency){
+        const ms = Date.now() - start
+        setFixedArrayValue(backend.latencies, ms, 10, backend.requestCount)
+        scoreLatency(backend)
+      }
+
+      // save backend stats every 3s
+      /*if(!backend.lastSaved || (Date.now() - backend.lastSaved) > 3000){
+
+      }*/
+
+      if (resp.status >= 500 && resp.status < 600) {
+        backend.lastError = Date.now()
+        // always recompute score on errors
+        scoreHealth(backend)
+
+        // clear out response to trigger retry
+        if (canRetry(req, resp)) {
+          continue
+        }
+      }
+
+      return resp
+    }
+
+    return proxyError
+  }
+
+  const balancer = Object.assign(fn, {
+    backends: tracked,
+    updateBackends: (backends: FetchFunction[]) => balancer.backends = tracked = syncBackends(tracked, backends)
+  })
+
+  return balancer;
+}
+const proxyError = new Response("couldn't connect to origin", { status: 502 })
+
+export function syncBackends(current: Backend[], replacements: FetchFunction[]){
+  const idx = new Map<FetchFunction, Backend>()
+  for(const b of current){
+    idx.set(b.proxy, b)
+  }
+
+  const updated: Backend[] = []
+
+  for(const fn of replacements){
+    if (typeof fn !== "function") {
+      throw Error("Backend must be a fetch like function")
+    }
+    const b = idx.get(fn) || {
+      proxy: fn,
+      requestCount: 0,
+      scoredRequestCount: 0,
+      statuses: Array<number>(10),
+      latencies: Array<number>(10),
+      lastError: 0,
+      healthScore: 1,
+      latencyScore: 1,
+      errorCount: 0
+    }
+
+    updated.push(b)
+  }
+  return updated;
+}
+
+/**
+ * Represents a backend with health and statistics.
+ */
+export interface Backend {
+  proxy: (req: RequestInfo, init?: RequestInit | undefined) => Promise<Response>,
+  requestCount: 0,
+  scoredRequestCount: 0,
+  statuses: number[],
+  latencies: number[],
+  lastError: number,
+  healthScore: number,
+  latencyScore: number,
+  errorCount: 0,
+  lastSaved?: number
+}
+// compute a backend health score with time + status codes
+function scoreHealth(backend: Backend, errorBasis?: number) {
+  if (typeof errorBasis !== "number" && !errorBasis) errorBasis = Date.now()
+
+  const timeSinceError = (errorBasis - backend.lastError)
+  const statuses = backend.statuses
+  const timeWeight = (backend.lastError === 0 && 0) ||
+    ((timeSinceError < 1000) && 1) ||
+    ((timeSinceError < 3000) && 0.8) ||
+    ((timeSinceError < 5000) && 0.3) ||
+    ((timeSinceError < 10000) && 0.1) ||
+    0;
+  if (statuses.length == 0) return 0
+  let requests = 0
+  let errors = 0
+  for (let i = 0; i < statuses.length; i++) {
+    const status = statuses[i]
+    if (status && !isNaN(status)) {
+      requests += 1
+      if (status >= 500 && status < 600) {
+        errors += 1
+      }
+    }
+  }
+  const healthScore = (1 - (timeWeight * (errors / requests)))
+  backend.healthScore = healthScore
+  backend.scoredRequestCount = backend.requestCount
+  return healthScore
+}
+function scoreLatency(backend: Backend){
+  let total = 0
+  for(const l of backend.latencies){
+    total += l
+  }
+  const avgLatency = total / backend.latencies.length
+
+  backend.latencyScore = orderOfMagnitude(avgLatency)
+  return backend.latencyScore
+}
+function canRetry(req: Request, resp: Response) {
+  if (resp && resp.status < 500) return false // don't retry normal boring errors or success
+  if (req.method == "GET" || req.method == "HEAD") return true
+  return false
+}
+
+function chooseBackends(backends: Backend[], attempted?: Set<Backend>) {
+  let b1: Backend | undefined
+  let b2: Backend | undefined
+  for (let i = 0; i < backends.length; i++) {
+    const b = backends[i]
+    if (attempted && attempted.has(b)) continue;
+
+    if (!b1) {
+      b1 = b
+      continue
+    }
+    if (!b2) {
+      b2 = b
+      continue
+    }
+
+    const old1 = b1
+    b1 = bestBackend(b, b1)
+
+    if (old1 != b1) {
+      // b1 got replaced, make sure it's not better
+      b2 = bestBackend(old1, b2)
+    } else {
+      b2 = bestBackend(b, b2)
+    }
+  }
+
+  // if two best backends have different latency, use only the fastest one
+  if(b1 && b2 && b1.latencyScore < b2.latencyScore) return [b1]
+  if(b1 && b2 && b2.latencyScore < b1.latencyScore) return [b2]
+  
+  return [b1, b2]
+}
+
+function bestBackend(b1: Backend, b2: Backend) {
+  // simple health check before we compare latency
+  if(b1.healthScore < 0.85 && b2.healthScore > 0.85){
+    return b2
+  }
+  if(b2.healthScore < 0.85 && b1.healthScore > 0.85){
+    return b1
+  }
+  if (
+    b1.latencyScore < b2.latencyScore ||
+    (b1.latencyScore == b2.latencyScore && b1.requestCount < b2.requestCount)
+  ) {
+    return b1
+  }
+  return b2
+}
+
+function setFixedArrayValue<T>(arr: T[], value: T, maxLength: number, totalCount: number){
+  if(arr.length < maxLength){
+    arr.push(value)
+  }else{
+    arr[(totalCount- 1) % arr.length] = value
+  }
+}
+
+function orderOfMagnitude(value: number){
+  //https://stackoverflow.com/questions/23917074/javascript-flooring-number-to-order-of-magnitude
+  const order = Math.floor(Math.log(value) / Math.LN10)
+  return Math.pow(10, order)
+}
+
+/** @private */
+export const _internal = {
+  chooseBackends,
+  scoreHealth,
+  scoreLatency
+}
---
+++ test/balancer.spec.ts
@@ -0,0 +1,191 @@
+import { expect } from 'chai'
+import balancer, { _internal, Backend, syncBackends } from "../src/balancer"
+
+async function fakeFetch(req: RequestInfo, init?: RequestInit) {
+  return new Response("hi")
+}
+async function fakeFetchError(req: RequestInfo, init?: RequestInit) {
+  return new Response("nooooo", { status: 502 })
+}
+
+function healthy() {
+  return <Backend>{
+    proxy: fakeFetch.bind({}), // same function, different times
+    requestCount: 0,
+    statuses: [200, 200, 200],
+    lastError: 0,
+    healthScore: 1,
+    latencyScore: 1,
+    errorCount: 0
+  }
+}
+function slow(latencyScore?: number){
+  const b = healthy()
+  b.latencyScore = latencyScore || 10
+  return b
+}
+function unhealthy(score?: number) {
+  const b = healthy()
+  b.statuses.push(500, 500, 500)
+  b.healthScore = score || 0.5
+  return b
+}
+describe("balancing", () => {
+  describe("backend scoring", () => {
+    it("should score healthy backends high", () => {
+      const backend = healthy()
+      const score = _internal.scoreHealth(backend)
+
+      expect(score).to.eq(1)
+    })
+
+    it("should score unhealthy backends low", () => {
+      const backend = unhealthy()
+      const score = _internal.scoreHealth(backend, 0)
+
+      expect(score).to.eq(0.5)
+    })
+
+
+    it("should give less weight to older errors", () => {
+      const backend = unhealthy()
+      let score = _internal.scoreHealth(backend, backend.lastError + 999)
+      expect(score).to.eq(0.5)
+
+      score = _internal.scoreHealth(backend, backend.lastError + 2000) // 2s old error
+      expect(score).to.eq(0.6)
+
+      score = _internal.scoreHealth(backend, backend.lastError + 4000) // 4s old error
+      expect(score).to.eq(0.85)
+
+      score = _internal.scoreHealth(backend, backend.lastError + 9000) // 9s old error
+      expect(score).to.eq(0.95)
+    })
+
+    const latencies: { [key: string]: number[] } = {
+      "1": [1, 2, 8, 10, 5, 8, 9],
+      "10": [10, 12, 18, 19, 70, 8, 9],
+      "100": [100, 900, 758, 123, 204, 203]
+    }
+    for(const r of Object.getOwnPropertyNames(latencies)){
+      const v = parseInt(r)
+      const arr = latencies[v]
+      const average = avg(arr)
+      it(`should bucket ${average} average latencyScore: ${v}`, () => {
+        const backend = healthy()
+        backend.latencies = arr
+  
+        const score = _internal.scoreLatency(backend)
+        expect(score).to.eq(v)
+      })
+    }
+  })
+
+  describe("backend selection", () => {
+    it("should choose healthy backends first", () => {
+      const h = [healthy(), healthy()]
+      const backends = [unhealthy(), unhealthy(), unhealthy()].concat(h)
+      const [b1, b2] = _internal.chooseBackends(backends)
+
+      expect(h.find((e) => e === b1)).to.eq(b1, "Backend 1 should be in selected")
+      expect(h.find((e) => e === b2)).to.eq(b2, "Backend 2 should be in selected")
+      expect(b1).to.not.eq(b2, "Backend 1 and Backend 2 should be different")
+    })
+
+    it("should choose lowest latency backends first", () => {
+      const h = [healthy(), healthy()]
+      const s = slow()
+      const backends = [s, unhealthy(), unhealthy(), unhealthy()].concat(h)
+      const [b1, b2] = _internal.chooseBackends(backends)
+
+      expect(b1).to.not.eq(s, "Backend 1 should not be the slow one")
+      expect(b2).to.not.eq(s, "Backend 2 should not be the slow one")
+    })
+
+    it("should not choose a high latency backend", () => {
+      const h = [healthy()]
+      const s = slow()
+      const backends = [s].concat(h)
+      const [b1, b2] = _internal.chooseBackends(backends)
+
+      expect(b1).to.not.eq(s, "Backend 1 should not be the slow one")
+      expect(b2).to.not.eq(s, "Backend 2 should not be the slow one")
+    })
+
+    it("ignores backends that have been tried", () => {
+      const h = [healthy(), healthy()]
+      const backends = [unhealthy(), unhealthy(), unhealthy()].concat(h)
+      const attempted = new Set<Backend>(h)
+
+      const [b1, b2] = _internal.chooseBackends(backends, attempted)
+      expect(h.find((e) => e === b1)).to.eq(undefined, "Backend 1 should not be in selected")
+      expect(h.find((e) => e === b2)).to.eq(undefined, "Backend 2 should not be in selected")
+      expect(b1).to.not.eq(b2, "Backend 1 and Backend 2 should be different")
+    })
+  })
+
+  describe("backend stats", () => {
+    it("should store last 10 statuses", async () => {
+      const req = new Request("http://localhost/hello/")
+      const fn = balancer([fakeFetch])
+      const backend = fn.backends[0]
+      const statuses = Array<number>()
+      for (let i = 0; i < 20; i++) {
+        let resp = await fn(req)
+        statuses.push(resp.status)
+      }
+
+      expect(backend.statuses.length).to.eq(10)
+      expect(backend.statuses).to.deep.eq(statuses.slice(-10))
+      expect(backend.healthScore).to.eq(1)
+    })
+
+    it("should retry on failure", async () => {
+      const fn = balancer([
+        fakeFetchError.bind({}),
+        fakeFetchError.bind({}),
+        fakeFetch
+      ])
+      // lower health of the last backend so we try errors first
+      fn.backends[2].healthScore = 0.1
+
+      const resp = await fn("http://localhost/")
+      const used = fn.backends.filter((b) => b.requestCount > 0)
+
+      expect(resp.status).to.eq(200)
+      expect(await resp.text()).to.eq("hi")
+      expect(used.length).to.be.gte(2) // at least two backends hit
+    })
+  })
+
+  describe("backend changes", () => {
+    it("should keep existing backends when possible", async () => {
+      const original = [healthy(), unhealthy(), healthy()]
+      const updated = [original[1].proxy, healthy().proxy, original[0].proxy, healthy().proxy]
+
+      const backends = syncBackends(original, updated)
+
+      expect(backends.length).to.eq(updated.length)
+
+      for(const i of [1, 0]){
+        expect(backends).to.include(original[i])
+      }
+    })
+
+    it('balancer should use new backends', async () => {
+      const fn = balancer([fakeFetch])
+      const original = fn.backends;
+      fn.updateBackends([healthy().proxy, healthy().proxy])
+  
+      const updated = fn.backends;
+  
+      expect(updated.length).to.eq(2)
+      expect(updated).to.not.eq(original);
+    })
+  })
+})
+
+function avg(array: number[]){
+  const sum = array.reduce((a,b) => a + b, 0)
+  return sum / array.length
+}
